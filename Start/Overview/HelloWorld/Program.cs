957d74d1801efb30a96bc7de171acde85ef5890c
JosephLaMantia <joeylamaxd@gmail.com>
2022-06-06 09:42:15 -0400

    Hello world program complete

---
+++ Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string str = Console.ReadLine();
+Console.WriteLine("Why, hello there " + str);
+
+
